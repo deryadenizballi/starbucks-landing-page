5c774f3d1b0ee44831add1bf2c8a64c27a7b1e04
deryadenizballi <dderyaaballi@gmail.com>
2020-12-15 01:05:07 +0300

    first versiyon

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src=anything;
+}
+
+function changecolor(color){
+    const circle =document.querySelector('.circle');
+    circle.style.background=color;
+}
